e31379b98e86de1eb6eab63b0676cbb5a771ac0f
James Reynolds (macbook) <james.reynolds@monash.edu>
2023-12-19 17:50:05 +1100

    Run through tidyverse styler

--- R/departures.R
+++ R/departures.R
@@ -9,45 +9,49 @@
 #' @return A number.
 #'
 #' @examples
-
 library(tidyverse)
 library(tidytransit)
 
 #' @export
 departures <- function(gtfs = gtfs,
-                               stop_ids = stop_ids,
-                               date_as_ymd = date_as_ymd,
-                               start_as_hms = lubridate::hms("-1:0:0"),
-                               end_as_hms = lubridate::hms("48:00:00")) {
+                       stop_ids = stop_ids,
+                       date_as_ymd = date_as_ymd,
+                       start_as_hms = lubridate::hms("-1:0:0"),
+                       end_as_hms = lubridate::hms("48:00:00")) {
+  # This is based on the tidytransit vignette on producing a departure timetable at https://r-transit.github.io/tidytransit/articles/timetable.html
+  # Trips departing from stop-----------------------------------
+  # Join to the selected stop_ids the trip columns: route_id, service_id, trip_headsign, and trip_origin. Because stop_ids and trips are linked via the stop_times data frame, we do this by joining the stop_ids we’ve selected to the stop_times data frame and then to the trips data frame.
 
-#This is based on the tidytransit vignette on producing a departure timetable at https://r-transit.github.io/tidytransit/articles/timetable.html
-#Trips departing from stop-----------------------------------
-#Join to the selected stop_ids the trip columns: route_id, service_id, trip_headsign, and trip_origin. Because stop_ids and trips are linked via the stop_times data frame, we do this by joining the stop_ids we’ve selected to the stop_times data frame and then to the trips data frame.
+  departures <- stop_ids %>%
+    dplyr::inner_join(
+      gtfs$stop_times %>%
+        dplyr::select(
+          trip_id, arrival_time,
+          departure_time, stop_id
+        ),
+      by = "stop_id"
+    )
 
-departures <- stop_ids %>%
-  dplyr::inner_join(gtfs$stop_times %>%
-               dplyr::select(trip_id, arrival_time,
-                      departure_time, stop_id),
-             by = "stop_id")
+  departures <- departures %>%
+    dplyr::left_join(
+      gtfs$trips %>%
+        dplyr::select(
+          trip_id, route_id,
+          service_id
+        ),
+      by = "trip_id"
+    )
 
-departures <- departures %>%
-  dplyr::left_join(gtfs$trips %>%
-              dplyr::select(trip_id, route_id,
-                     service_id),
-            by = "trip_id")
+  # Extract a single day--------------------------------
+  services_on_date <- gtfs$.$dates_services %>%
+    dplyr::filter(date == date_as_ymd) %>%
+    dplyr::select(service_id)
 
-#Extract a single day--------------------------------
-services_on_date <- gtfs$.$dates_services %>%
-  dplyr::filter(date == date_as_ymd) %>% dplyr::select(service_id)
+  departures_on_date <- departures %>%
+    dplyr::inner_join(services_on_date, by = "service_id")
 
-departures_on_date <- departures %>%
-  dplyr::inner_join(services_on_date, by = "service_id")
-
-#return count of number of rows in departures_on_date dataframe between the start and end times
-nrow(departures_on_date %>%
+  # return count of number of rows in departures_on_date dataframe between the start and end times
+  nrow(departures_on_date %>%
     dplyr::filter(departure_time >= start_hms) %>%
-    dplyr::filter(departure_time < end_hms)
-  )
+    dplyr::filter(departure_time < end_hms))
 }
-
-
