74ae1836bda8a30a09863b30d1040dba9cf246cb
James Reynolds (macbook) <james.reynolds@monash.edu>
2024-05-26 22:06:21 +1000

    Date issue fixed.

--- R/SI_by_day_hour_and_route_type.R
+++ R/SI_by_day_hour_and_route_type.R
@@ -1,3 +1,48 @@
+#' Calculate SI for each: route type, hour (of service), day between 
+#' a start date and an end date (inclusive), and area_of_interest
+#'
+#' @param list_gtfs Output of the gtfs_by_route_type function, 
+#' a list (by route_type) of tidygtfs objects
+#' @param stops_in_or_near_areas Output of the stops_in_walk_dist function. 
+#' A list (by route_type) of stops and areas within walking distance of each other 
+#' and the associated area_terms of SI calculation
+#' @param start_date_ymd the first day of the period of interest
+#' @param end_date_ymd the last day of the period of interest
+#' @param verbose Print details of progress during calculations
+#'
+#' @return A dataframe with SI for each combination of route type, area of interest, 
+#' hour of service and date. 
+#' @export
+#'
+#' 
+#' @examples
+#' ####-----first load all the inputs
+#' #load the revised mornington GTFS data
+#' list_gtfs = gtfssupplyindex:::gtfs_by_route_type(system.file(
+#' "extdata/mornington180109",
+#' "gtfs.zip", 
+#' package = "gtfssupplyindex", 
+#' mustWork = TRUE))
+#' 
+#' areas_of_interest <- load_areas_of_interest(absmapsdata::sa22021 %>% 
+#'             dplyr::filter(sa3_name_2021 ==  "Mornington Peninsula") %>% 
+#'                                              dplyr::select(sa2_code_2021),  
+#'                                            area_id_field = "sa2_code_2021") 
+#' buffer_distance <- gtfssupplyindex:::load_buffer_zones()
+#' 
+#' stops_in_or_near_areas <- gtfssupplyindex:::stops_in_walk_dist(
+#' list_gtfs = list_gtfs, 
+#' areas_of_interest = areas_of_interest,
+#' EPSG_for_transform = 28355,
+#' verbose = FALSE
+#' )
+#' 
+#' SI_by_day_hour_and_route_type(
+#' list_gtfs, 
+#' stops_in_or_near_areas, 
+#' "2018-12-23",
+#' "2018-12-30",
+#' verbose = FALSE)
 
 SI_by_day_hour_and_route_type <- function(
     list_gtfs, 
@@ -26,8 +71,8 @@ SI_by_day_hour_and_route_type <- function(
   
   # loop SI calculation over days between start_date_ymd and end_date_ymd   
   
-  
-  for(i in seq(as.Date(start_date_ymd), as.Date(end_date_ymd), by="days")){
+  dates <- seq(as.Date(start_date_ymd), as.Date(end_date_ymd), by="days")
+  for(i in as.list(dates)){
  
     # create table for output  
     si_by_area_route_and_hour <- data.frame()
